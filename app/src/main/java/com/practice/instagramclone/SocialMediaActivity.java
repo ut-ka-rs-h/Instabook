c4ece3f1a80f7d0a06314483e370934e0e1c3410
Utkarsh <utkarshsrivastav233@gmail.com>
2020-06-29 19:19:38 +0530

    Tab Layout is Set up...

--- app/src/main/java/com/practice/instagramclone/SocialMediaActivity.java
+++ app/src/main/java/com/practice/instagramclone/SocialMediaActivity.java
@@ -1,15 +1,37 @@
 package com.practice.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class SocialMediaActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private TabAdapter tabAdapter;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
         setTitle(R.string.app_name);
+
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager(), 0);
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, true);
+
     }
 }
